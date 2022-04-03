489dc290bd41a5fabb7c09a6a48c8a8be9370c40
Nadilson <nadilson@protonmail.com>
2022-04-03 04:25:59 -0300

    Add Header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import Header from "../components/Header";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <Header />
+      <main className="container">
+        <Component {...pageProps} />
+      </main>
     </>
-  );
+  )
 }
 
 export default MyApp;
